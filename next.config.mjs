f79933c956072f12299ac38b213dc04e7918e208
Alice <dali.retta@gmail.com>
2026-06-12 17:08:33 -0600

    chore: configure next.config for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  output: 'export',
+  basePath: isProd ? '/Hongos' : '',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -10,7 +14,6 @@ const nextConfig = {
       },
     ],
   },
-
 };
 
 export default nextConfig;
